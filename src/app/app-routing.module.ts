fe79547ab8921a2bb27bf1538ead647fe7ecef81
kreuzerk <kevin.kreuzer90@icloud.com>
2021-02-04 08:48:42 +0100

    feat: 🎸 add sidemenu items and global styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-const routes: Routes = [{ path: 'hades', loadChildren: () => import('./features/hades/hades.module').then(m => m.HadesModule) }, { path: 'minotaur', loadChildren: () => import('./features/minotaur/minotaur.module').then(m => m.MinotaurModule) }, { path: 'achilles', loadChildren: () => import('./features/achilles/achilles.module').then(m => m.AchillesModule) }];
+const routes: Routes = [{
+  path: 'hades',
+  loadChildren: () => import('./features/hades/hades.module').then(m => m.HadesModule)
+}, {path: 'minotaur', loadChildren: () => import('./features/minotaur/minotaur.module').then(m => m.MinotaurModule)}, {
+  path: 'achilles',
+  loadChildren: () => import('./features/achilles/achilles.module').then(m => m.AchillesModule),
+},
+  {path: '', redirectTo: 'hades', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
